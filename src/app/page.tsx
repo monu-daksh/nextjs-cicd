0da2fc935ec98a43d51afb75e261a25238d3469f
monudaksh <monu.monu@soprasteria.com>
2025-10-18 20:52:18 +0530

    added comments in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import SignupForm from "@/feature/AuthModule/Components/SignupForm";
 import Image from "next/image";
 
+// Home page
 export default function Home() {
   return (
     <SignupForm  />
